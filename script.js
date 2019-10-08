439137906c5b48f966c4dec4445a8ce52599b433
Hubby999 <55356461+Hubby999@users.noreply.github.com>
2019-10-09 05:49:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
-function pasanganTerbesar(num) {
-	var number = String(num).split('');
-	var angka = [];
-	for (var i = 0; i < number.length; i++) {
-		angka[i] = number[i] + number[i + 1];
+function pasanganTerkuat(num) {
+	var no = String(num).split('');
+	var bilangan = [];
+	for (var i = 0; i < no.length; i++) {
+		bilangan[i] = no[i] + no[i + 1];
 	}
-	var digit = angka[0];
-	for (var j = 1; j < angka.length; j++){
-		if (angka[j] > digit){
-			digit = angka[j]
+	var digit = bilangan[0];
+	for (var j = 1; j < bilangan.length; j++){
+		if (bilangan[j] > digit){
+			digit = bilangan[j]
 		}
 	}
 	return digit;
 }
-console.log(pasanganTerbesar(641573)); // 73
-console.log(pasanganTerbesar(12783456)); // 83
-console.log(pasanganTerbesar(910233)); // 91
-console.log(pasanganTerbesar(71856421)); // 85
-console.log(pasanganTerbesar(79918293)); // 99
+console.log(pasanganTerkuat(641573)); // 73
+console.log(pasanganTerkuat(12783456)); // 83
+console.log(pasanganTerkuat(910233)); // 91
+console.log(pasanganTerkuat(71856421)); // 85
+console.log(pasanganTerkuat(79918293)); // 99
