f66da94b1d543870f470d809027a2b275f08b7ef
MAS FIAN 77 <masfian77@gmail.com>
2020-10-20 20:15:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-function pasanganTerbesar(num) {
+function biggestCouple(num) {
 	var number = String(num).split('');
 	var angka = [];
 	for (var i = 0; i < number.length; i++) {
@@ -13,8 +12,9 @@ function pasanganTerbesar(num) {
 	}
 	return digit;
 }
-console.log(pasanganTerbesar(641573)); // 73
-console.log(pasanganTerbesar(12783456)); // 83
-console.log(pasanganTerbesar(910233)); // 91
-console.log(pasanganTerbesar(71856421)); // 85
-console.log(pasanganTerbesar(79918293)); // 99
+
+console.log(biggestCouple(641573)); 
+console.log(biggestCouple(12783456));
+console.log(biggestCouple(910233));
+console.log(biggestCouple(71856421));
+console.log(biggestCouple(79918293));
